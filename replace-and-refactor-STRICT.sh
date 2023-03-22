802a6cd7123926fba65585661de29869eeed0f10
moueza <>
2023-03-22 18:11:31 +0100

    fix: save before test

--- replace-and-refactor-STRICT.sh
+++ replace-and-refactor-STRICT.sh
@@ -4,7 +4,15 @@
 #$1 must finish by dot for exact
 cd ~/POUB/replace-and-refactor-angular/TEST
 entry1=$1
-echo "dollar1=$entry1"
+entry1="test2."
+
+echo "entry1=$entry1"
+
+
+prefix = `echo $entry1| cut -c 2-1000`
+    echo "prefix=$prefix"
+
+
 #find .js  --exec
 
 find . -exec sed -i 's/tests1/TEST1/g' test2.ts \;
