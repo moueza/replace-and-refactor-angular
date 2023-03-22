3b48324432f07f0d3fb561ed35479aa51580d88a
moueza <>
2023-03-22 17:15:14 +0100

    feat: delete to shrink

---
+++ replace-and-refactor-STRICT.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#pre existing so extension available
+$0
+find .js  --exec 
---
+++ replace-and-refactor-angular/src/main/java/homniserv/replace_and_refactor_angular/App.java
@@ -0,0 +1,13 @@
+package homniserv.replace_and_refactor_angular;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
